84a5c57cabe87b9facabfcd2a8754d2604f7ab68
Ivan Kolpakov <i.kolpakov@x5.ru>
2018-08-15 12:57:09 +0300

    add pictures

--- src/main/java/ru/airiva/DataSourceImpl.java
+++ src/main/java/ru/airiva/DataSourceImpl.java
@@ -30,6 +30,21 @@ public class DataSourceImpl implements JRDataSource {
     private static final String WEIGHT_PROMO_PRICE = "weightPromoPrice";
     private static final String WEIGHT_NORMAL_PRICE = "weightNormalPrice";
     private static final String EAN_CONTENT = "eanContent";
+    private static final String TEXT_101 = "text101";
+    private static final String TEXT_102 = "text102";
+    private static final String TEXT_103 = "text103";
+    private static final String TEXT_104 = "text104";
+    private static final String TEXT_105 = "text105";
+    private static final String TEXT_106 = "text106";
+    private static final String TEXT_107 = "text107";
+    private static final String TEXT_108 = "text108";
+    private static final String TRADEMARK = "trademark";
+    private static final String WEIGHT_TYPE_SUPPLIER_INFO = "weightTypeSupplierInfo";
+    private static final String PLANOGRAMA = "planograma";
+    private static final String BASE_QTY = "baseQty";
+    private static final String DISCOUNT_VALUE = "discountValue";
+    private static final String SPECIAL_PRICE = "specialPrice";
+    private static final String PACK_PRICE = "packPrice";
 
     private DataSourceImpl() {
         fields.put(BRAND_LOGO, new ImageWrapper("logo.jpg"));
@@ -39,17 +54,32 @@ public class DataSourceImpl implements JRDataSource {
         fields.put(TEXT_4, "");
         fields.put(PRICE, 19999.95d);
         fields.put(NORMAL_PRICE, 59999.99d);
-        fields.put(PROMO_PRICE, 399.88d);
+        fields.put(PROMO_PRICE, 19999.88d);
         fields.put(EAN, "4813445000650");
         fields.put(MANUFACTURER, "КФ Ударница");
         fields.put(MAIN_ITEM_ID, "10795");
         fields.put(IS_TPC, Boolean.TRUE);
         fields.put(IS_PROMO, Boolean.TRUE);
         fields.put(WEIGHT_TYPE_BUTTON, "999");
-        fields.put(WEIGHT_PRICE, "3333.56");
-        fields.put(WEIGHT_PROMO_PRICE, "4444.66");
+        fields.put(WEIGHT_PRICE, "33333.56");
+        fields.put(WEIGHT_PROMO_PRICE, "44444.66");
         fields.put(WEIGHT_NORMAL_PRICE, "8888.44");
         fields.put(EAN_CONTENT, "%0001079519999950");
+        fields.put(TEXT_101, "12345678901234567890123456789012345678901234567890");
+        fields.put(TEXT_102, "12345678901234567890123456789012345678901234567890");
+        fields.put(TEXT_103, "12345678901234567890123456789012345678901234567890");
+        fields.put(TEXT_104, "12345678901234567890123456789012345678901234567890");
+        fields.put(TEXT_105, "12345678901234567890123456789012345678901234567890");
+        fields.put(TEXT_106, "12345678901234567890123456789012345678901234567890");
+        fields.put(TEXT_107, "12345678901234567890123456789012345678901234567890");
+        fields.put(TEXT_108, "12345678901234567890123456789012345678901234567890");
+        fields.put(TRADEMARK, "Торговая марка");
+        fields.put(WEIGHT_TYPE_SUPPLIER_INFO, "999");
+        fields.put(PLANOGRAMA, "002.001.79.10");
+        fields.put(DISCOUNT_VALUE, 10);
+        fields.put(BASE_QTY, 10);
+        fields.put(SPECIAL_PRICE, "899.99");
+        fields.put(PACK_PRICE, "8990.00");
     }
 
     private int counter = -1;
